b4459097049b4630de6db0c62dc2d97a19de3d33
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-06-16 11:20:09 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,8 +1,8 @@
 export const OWNER = "Yidadaa";
-export const REPO = "Ai 百晓生";
-export const REPO_URL = `https://imgbed.link/file/22797`;
-export const ISSUE_URL = `https://imgbed.link/file/22797`;
+export const REPO = "ChatGPT Pro";
+export const REPO_URL = `http://wj.lzlspyxgs.asia/`;
+export const ISSUE_URL = `http://wj.lzlspyxgs.asia/`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
-export const FETCH_COMMIT_URL = `https://imgbed.link/file/22797`;
-export const FETCH_TAG_URL = `https://imgbed.link/file/22797`;
+export const FETCH_COMMIT_URL = `http://wj.lzlspyxgs.asia/`;
+export const FETCH_TAG_URL = `http://wj.lzlspyxgs.asia/`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
